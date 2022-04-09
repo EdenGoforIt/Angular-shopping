698af81bab3a8ee3e5c2fda4c5b4fddd94cb58c4
EdenPark <edengoforit@gmail.com>
2022-04-10 09:54:15 +1200

    Fixed bootstrap dropdown issue

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,13 @@ import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.com
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    RecipeListComponent,
     RecipesComponent,
     RecipeDetailComponent,
     RecipeItemComponent,
